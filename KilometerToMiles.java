3b75fcdc36b99e855ff325665d90ff7258bff187
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-02-28 21:06:21 +0530

    [Naman Malhotra] ADD: Converting Kilometers to Miles

---
+++ KilometerToMiles.java
@@ -0,0 +1,7 @@
+public class KilometerToMiles {
+    public static void main(String[] args) {
+        double kilometerDistance = 10.8;
+        
+        System.out.println("The distance " + kilometerDistance + " km in miles is " + kilometerDistance/1.6);
+    }
+}
